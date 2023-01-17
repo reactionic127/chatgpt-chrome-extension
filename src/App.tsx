0e70bbec30cf7e089ecbb9fb43c6c889cdb2f8cf
Richard Jin <richard.gill127@gmail.com>
2023-01-17 12:22:51 -0800

    removed useless css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import { DOMMessage, DOMMessageResponse } from './types';
+import './App.css';
 
 function App() {
   const [title, setTitle] = React.useState('');
@@ -32,37 +32,13 @@ function App() {
     });
   });
 
+  console.log(title, headlines);
+
   return (
     <div className="App">
-      <h1>ChatGPT Extension built with React!</h1>
-
-      <ul className="SEOForm">
-        <li className="SEOValidation">
-          <div className="SEOValidationField">
-            <span className="SEOValidationFieldTitle">Title</span>
-            <span className={`SEOValidationFieldStatus ${title.length < 30 || title.length > 65 ? 'Error' : 'Ok'}`}>
-              {title.length} Characters
-            </span>
-          </div>
-          <div className="SEOVAlidationFieldValue">
-            {title}
-          </div>
-        </li>
-
-        <li className="SEOValidation">
-          <div className="SEOValidationField">
-            <span className="SEOValidationFieldTitle">Main Heading</span>
-            <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
-              {headlines.length}
-            </span>
-          </div>
-          <div className="SEOVAlidationFieldValue">
-            <ul>
-              {headlines.map((headline, index) => (<li key={index}>{headline}</li>))}
-            </ul>
-          </div>
-        </li>
-      </ul>
+      <div className="header">
+        <h3>ChatGPT Chrome Extension</h3>
+      </div>
     </div>
   );
 }
